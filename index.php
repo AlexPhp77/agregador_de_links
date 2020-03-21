5b242a7a42b439a1634c0a97ace6e2cb6c5499a5
Álex R S <alex.rai@hotmail.com>
2020-03-21 19:51:56 -0300

    Estrutura do topo

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<head lang="pt-br">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">  
+  <title>Agregador de Links</title>
+</head>
+<body> 
+
+  <!--Topo-->
+  <header>
+   
+    <nav class="menu">  
+      <ul class="logo"><li><a href="#">Agregador de Links</a></li></ul>    
+      <ul>        
+        <li><a href="#">Acesso</a></li>
+        <li><a href="#">Cadastre-se</a></li>
+      </ul>        
+    </nav>
+   
+    <div class="jumbotron" style="background-color:#ffe484">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-outline-success" href="#" role="button">Anuncie</a>
+      </p>
+    </div>
+
+  </header>
+  
+  <!--Conteúdo-->
+  <main class="conteudo">          
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>  
+    <div class="box"></div>  
+    <div class="box"></div>  
+  </main>
+  
+  <script src="assets/js/jquery-3.4.1.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>  
+</body>
+</html>
